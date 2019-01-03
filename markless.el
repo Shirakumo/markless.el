ee23632f16c1d3ffa3b868b6c9500229c9c0b868
Shinmera <shinmera@tymoon.eu>
2019-01-03 19:29:05 +0100

    Various fixes, add support for code blocks

--- markless.el
+++ markless.el
@@ -57,7 +57,7 @@ NAME PROP DOC, shut up, checkdoc."
 (--markless-defface markless-bold-face (:inherit bold))
 (--markless-defface markless-underline-face (:underline t))
 (--markless-defface markless-strikethrough-face (:strike-through t))
-(--markless-defface markless-literal-face (:inherit (fixed-pitch font-lock-constant-face)))
+(--markless-defface markless-literal-face (:inherit (font-lock-constant-face fixed-pitch)))
 (--markless-defface markless-url-face (:inherit link))
 (--markless-defface markless-spoiler-face (:background "black" :foreground "black"))
 (--markless-defface markless-quote-source-face (:inherit font-lock-variable-name-face))
@@ -72,26 +72,26 @@ NAME PROP DOC, shut up, checkdoc."
 (--markless-defface markless-footnote-face (:inherit markless-quote-face))
 (--markless-defface markless-highlight-face (:inherit highlight))
 
-(defun markless-mark (start end prop)
+(cl-defun markless-mark (start end prop)
   "Shorthand to mark up a text between START and END with PROP."
   (if (or (symbolp prop) (keywordp (first prop)))
       (add-face-text-property start end prop)
-      (add-text-properties start end prop))))
+      (add-text-properties start end prop)))
 
-(defun markless-match (string)
+(cl-defun markless-match (string)
   "Attempt to match STRING.  If successful, return t.
 
 This does not alter the point."
-  (when (< (+ (point) (length string)) (point-max))
+  (when (<= (+ (point) (length string)) (point-max))
     (cl-loop for i from (point)
              for char across string
              always (= char (char-after i)))))
 
-(defun markless-num-p (point)
+(cl-defun markless-num-p (point)
   "Return t if the char at POINT is a numeric char."
   (<= ?0 (char-after point) ?9))
 
-(defun markless-inline-directive (pre post prop)
+(cl-defun markless-inline-directive (pre post prop)
   "Process the inline directive recursively.
 
 If PRE matches, recurses until POST is found.
@@ -108,13 +108,27 @@ Marks PRE and POST as markup and the content with PROP."
              (markless-mark (+ start (length pre)) (point) prop)))
       t)))
 
-(defun markless-parse-option (option)
+(cl-defun markless-parse-option (option)
   "Parse the compound OPTION to a face."
-  (cond ((string= option "bold") 'markless-bold-face)
+  (cond ;; Font style names
+        ((string= option "bold") 'markless-bold-face)
         ((string= option "italic") 'markless-italic-face)
         ((string= option "underline") 'markless-underline-face)
         ((string= option "strikethrough") 'markless-strikethrough-face)
         ((string= option "spoiler") 'markless-spoiler-face)
+        ;; Colour names
+        ((color-defined-p option)
+         `(:foreground ,option))
+        ;; Font size names
+        ((string= option "microscopic") `(:height 0.25))
+        ((string= option "tiny") `(:height 0.5))
+        ((string= option "small") `(:height 0.8))
+        ((string= option "normal") `(:height 1.0))
+        ((string= option "big") `(:height 1.5))
+        ((string= option "large") `(:height 2.0))
+        ((string= option "huge") `(:height 2.5))
+        ((string= option "gigantic") `(:height 4.0))
+        ;; Complex options
         ((string-prefix-p "font" option)
          `(:family ,(subseq option 5)))
         ((string-prefix-p "color" option)
@@ -131,31 +145,21 @@ Marks PRE and POST as markup and the content with PROP."
                `(:height ,(round (* 10 (string-to-number size)))))))
         ((or (string-prefix-p "link" option)
              (string-match markless-url-regex option))
-         'markless-url-face)
-        ((color-defined-p option)
-         `(:foreground ,option))
-        ((string= option "microscopic") `(:height 0.25))
-        ((string= option "tiny") `(:height 0.5))
-        ((string= option "small") `(:height 0.8))
-        ((string= option "normal") `(:height 1.0))
-        ((string= option "big") `(:height 1.5))
-        ((string= option "large") `(:height 2.0))
-        ((string= option "huge") `(:height 2.5))
-        ((string= option "gigantic") `(:height 4.0))))
+         'markless-url-face)))
 
-(defun markless-compute-options-faces (options)
+(cl-defun markless-compute-options-faces (options)
   "Parse the list of compound OPTIONS to a list of faces."
   (cl-loop for option in options
            for face = (markless-parse-option option)
            when face collect face))
 
-(defun markless-match-inline (&optional end)
+(cl-defun markless-match-inline (&optional end)
   "Markup inline directives until the END is matched or until the end of line is found."
   (cl-loop
    (or (when (= (point) (point-at-eol))
-         (return nil))
+         (return-from markless-match-inline nil))
        (when (and end (markless-match end))
-         (return t))
+         (return-from markless-match-inline t))
        (markless-inline-directive "**" "**" 'markless-bold-face)
        (markless-inline-directive "//" "//" 'markless-italic-face)
        (markless-inline-directive "__" "__" 'markless-underline-face)
@@ -176,18 +180,50 @@ Marks PRE and POST as markup and the content with PROP."
                    (markless-mark (1+ start) end face)))))))
        (forward-char))))
 
-(defun markless-match-block ()
+(cl-defun markless-in-code-block-p ()
+  "Return t if the `point' is within a code block."
+  (let ((middle (point-at-bol))
+        (block-size nil))
+    (cl-flet ((find-block-size ()
+               (let ((start (point)))
+                 (cl-loop while (and (< (point) (point-max)) (= ?: (char-after (point))))
+                          do (forward-char))
+                 (- (point) start))))
+      (save-excursion
+       (goto-char (point-min))
+       (cl-loop until (or (and (not block-size)
+                               (<= middle (point)))
+                          (< middle (point)))
+                do (when (markless-match "::")
+                     (cond ((null block-size)
+                            (setq block-size (find-block-size)))
+                           ((and (= block-size (find-block-size))
+                                 (= (point) (point-at-eol)))
+                            (setq block-size nil))))
+                (forward-line))
+       (not (null block-size))))))
+
+(cl-defun markless-header-scale-factor (depth)
+  "Return the relative font height scaling factor for a header of DEPTH."
+  (+ 1.0 (/ (expt (max 0 (- 6 depth)) 2)
+            10.0)))
+
+(cl-defun markless-match-block (end)
   "Markup block directives until the end of the line or `point-max'."
-  (cl-loop while (and (< (point) (point-max)) (= ?  (char-after (point))))
+  (when (markless-in-code-block-p)
+    (markless-mark (point) (point-at-eol) 'markless-literal-face)
+    (move-end-of-line 1)
+    (return-from markless-match-block))
+  (cl-loop while (and (< (point) end) (= ?  (char-after (point))))
            do (forward-char))
   (cond ((markless-match "#")
          (let ((start (point)))
            (cl-loop while (and (< (point) (point-max)) (= ?# (char-after (point))))
                     do (forward-char))
            (cond ((= ?  (char-after (point)))
-                  (let ((size (- (point) start)))
+                  (let ((depth (- (point) start)))
                     (markless-mark start (point) 'markless-markup-face)
-                    (markless-mark start (point-at-eol) (list :height (+ 1.0 (/ (max 0 (- 6 size)) 3.0))))
+                    (markless-mark start (point-at-eol) (list :height (markless-header-scale-factor depth)))
                     (markless-match-inline)))
                  (t
                   (goto-char start)
@@ -199,7 +235,7 @@ Marks PRE and POST as markup and the content with PROP."
         ((markless-match "| ")
          (markless-mark (point) (point-at-eol) 'markless-quote-face)
          (forward-char 2)
-         (markless-match-block))
+         (markless-match-block end))
         ((markless-match "[ ")
          (let ((start (point)))
            (move-end-of-line 1)
@@ -213,8 +249,9 @@ Marks PRE and POST as markup and the content with PROP."
            (move-end-of-line 1)
            (markless-mark start (point) 'markless-comment-face)))
         ((markless-match "::")
-         ;; FIXME: Implement this
-         )
+         (let ((start (point)))
+           (move-end-of-line 1)
+           (markless-mark start (point) 'markless-markup-face)))
         ((and (markless-match "[") (markless-num-p (1+ (point))))
          (let ((start (point)))
            (forward-char)
@@ -230,7 +267,7 @@ Marks PRE and POST as markup and the content with PROP."
         ((markless-match "- ")
          (markless-mark (point) (+ 2 (point)) 'markless-list-mark-face)
          (forward-char 2)
-         (markless-match-block))
+         (markless-match-block end))
         ((markless-num-p (point))
          (let ((start (point)))
            (cl-loop while (and (< (point) (point-max)) (markless-num-p (point)))
@@ -238,24 +275,24 @@ Marks PRE and POST as markup and the content with PROP."
            (cond ((= ?. (char-after (point)))
                   (forward-char)
                   (markless-mark start (point) 'markless-list-mark-face)
-                  (markless-match-block))
+                  (markless-match-block end))
                  (t
                   (goto-char start)
                   (markless-match-inline)))))
         (t
          (markless-match-inline))))
 
-(defun markless-fontify (end)
+(cl-defun markless-fontify (end)
   "Generate markup for Markless until END."
   (cl-loop while (< (point) end)
-           do (markless-match-block)
+           do (markless-match-block end)
            (when (< (point) end)
              (forward-char))))
 
 (defconst markless-url-regex "[[:alpha:]][[:alnum:]+\\-.]*://[[:alnum:]$\\-_.+!*'()&,/:;=?@%#\\\\]+"
   "Regex to match URLs as specified by Markless.")
 
-(defun markless-fontify-url (end)
+(cl-defun markless-fontify-url (end)
   "Markup URLs until the END."
   (when (re-search-forward markless-url-regex end t)
     (goto-char (1+ (match-end 0)))
@@ -267,11 +304,11 @@ Marks PRE and POST as markup and the content with PROP."
       (add-text-properties (match-beginning 0) (match-end 0) props)
       t)))
 
-(defun markless-follow-link-at-point ()
+(cl-defun markless-follow-link-at-point ()
   "Follow the URL at the current point, if any."
   (interactive)
   (if (thing-at-point-looking-at markless-url-regex)
-      (let* ((url (message (match-string 0)))
+      (let* ((url (match-string 0))
              (struct (url-generic-parse-url url)))
         (if (url-fullness struct)
             (browse-url url)
@@ -279,7 +316,7 @@ Marks PRE and POST as markup and the content with PROP."
               (when (and file (< 0 (length file))) (find-file file)))))
       (user-error "Point is not at a link or URL")))
 
-(defun markless-at-word-p ()
+(cl-defun markless-at-word-p ()
   "Return t if the current point is a word that should be spell-checked."
   (not (let ((faces (get-text-property (point) 'face)))
          (unless (listp faces) (setq faces (list faces)))
@@ -305,9 +342,12 @@ Marks PRE and POST as markup and the content with PROP."
                                       (markless-fontify))
   "Font lock keywords for Markless mode.")
 
+;;;###autoload
 (define-derived-mode markless-mode text-mode "Markless"
   "Major mode for Markless documents."
-  (setq font-lock-defaults '(markless-font-lock-keywords))
+  (setq font-lock-defaults '(markless-font-lock-keywords
+                             nil nil nil nil
+                             (font-lock-multiline . t)))
   (setq-local flyspell-generic-check-word-predicate
               #'markless-at-word-p))
 
